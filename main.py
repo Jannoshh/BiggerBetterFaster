77f6a755a472a8b0fafca3ecd094f5d8a6d81e30
Jannes Elstner <janneselstner@gmail.com>
2023-09-07 23:06:54 +0200

    more modularity, refactoring

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import random
 from dataclasses import dataclass
-import os
-os.environ['OMP_NUM_THREADS'] = '1'
+from itertools import count
 
 import gymnasium as gym
 import numpy as np
-
 import torch
-import torch.nn.functional as F
+from einops import einops
 
 from models import DQN
 from replay_buffer import ReplayBuffer
@@ -53,7 +51,7 @@ def select_epsilon_greedy_action(model, state, epsilon):
     if random.random() < epsilon:
         return random.randint(0, n_actions - 1)
     else:
-        state = torch.FloatTensor(state).unsqueeze(0)
+        state = einops.rearrange(torch.FloatTensor(state), 'c h w -> 1 c h w')
         q_values = model(state)
         return q_values.argmax(dim=1).item()
 
@@ -68,52 +66,49 @@ replay_buffer = ReplayBuffer(net_config.buffer_size)
 optimizer = torch.optim.Adam(dqn.parameters(), lr=net_config.learning_rate)
 replay_buffer = ReplayBuffer(net_config.buffer_size)
 
+def optimize_model():
+    if len(replay_buffer) > net_config.batch_size:
+        batch = replay_buffer.sample(net_config.batch_size)
+        loss = compute_loss(batch)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
 def compute_loss(batch):
     states, actions, rewards, next_states, dones = batch
-    states = torch.FloatTensor(states)
-    actions = torch.LongTensor(actions)
-    rewards = torch.FloatTensor(rewards)
-    next_states = torch.FloatTensor(next_states)
-    mask = torch.FloatTensor(1 - np.array(dones, dtype=np.float32))
 
-    current_q_values = dqn(states).gather(1, actions.unsqueeze(-1)).squeeze(-1)
-    next_q_values = target_dqn(next_states).max(dim=1)[0]
-    target_q_values = rewards + net_config.gamma * next_q_values * mask
+    mask = 1 - dones
+    current_q_values = dqn(states).gather(1, einops.rearrange(actions, 'b -> b 1'))
+    next_q_values = target_dqn(next_states).max(1)[0]
+    target_q_values = rewards + net_config.gamma * next_q_values * mask.squeeze()
 
-    return F.mse_loss(current_q_values, target_q_values)
+    loss = ((current_q_values - target_q_values) ** 2).mean()
+    return loss
 
-NUM_EPISODES = 1000
-TARGET_UPDATE_INTERVAL = 1000
-EPSILON_DECAY_LAST_FRAME = 100000
-
-frame_idx = 0
+total_steps = 0
 all_rewards = []
 episode_reward = 0
 
-for episode in range(NUM_EPISODES):
+for episode in range(train_config.num_episodes):
     state, info = env.reset()  # Reset the environment
-    state = preprocess_state(state)  # Preprocess the initial state
+    state = preprocess_state(state)
     episode_reward = 0
 
-    while True:
-        epsilon = epsilon_decay(frame_idx, train_config.epsilon_start, train_config.epsilon_end, train_config.epsilon_decay_last_frame)
+    for episode_step in count():
+        epsilon = epsilon_decay(total_steps, train_config.epsilon_start, train_config.epsilon_end, train_config.epsilon_decay_last_frame)
         action = select_epsilon_greedy_action(dqn, state, epsilon)
         next_state, reward, done, _, _ = env.step(action)
-        next_state = preprocess_state(next_state)  # Preprocess the next state
+        next_state = preprocess_state(next_state)
         replay_buffer.push(state, action, reward, next_state, done)
 
         state = next_state
         episode_reward += reward
-        frame_idx += 1
+        total_steps += 1
 
-        if len(replay_buffer) > net_config.batch_size:
-            batch = replay_buffer.sample(net_config.batch_size)
-            loss = compute_loss(batch)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        optimize_model()
 
-        if frame_idx % TARGET_UPDATE_INTERVAL == 0:
+        if total_steps % train_config.target_update_interval == 0:
             target_dqn.load_state_dict(dqn.state_dict())
 
         if done:
