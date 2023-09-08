f478d6c50ae938b3ecbd882dea955c5e2ddc30f2
Jannes Elstner <janneselstner@gmail.com>
2023-09-08 12:23:24 +0200

    shrink and perturb, update horizon scheduler

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import cv2
 import einops
+import torch
 
 
 def preprocess_state(state):
@@ -23,4 +24,48 @@ class TargetNetworkUpdater:
     def soft_update(self):
         for target_param, source_param in zip(self.target_network.parameters(), self.source_network.parameters()):
             target_param.data.copy_(self.tau * source_param.data + (1.0 - self.tau) * target_param.data)
+
+
+def shrink_and_perturb_parameters(model, alpha):
+    for name, param in model.named_parameters():
+        if 'encoder' in name:  # Assuming 'encoder' is part of the name for encoder layers
+            phi = torch.randn_like(param).normal_(0, 0.01)  # Assuming a normal initializer with mean 0 and std 0.01
+            param.data = alpha * param.data + (1 - alpha) * phi
+        elif 'final' in name:
+            phi = torch.randn_like(param).normal_(0, 0.01)  # Assuming a normal initializer with mean 0 and std 0.01
+            param.data = phi
+
+
+def exponential_decay_scheduler(
+    decay_period, warmup_steps, initial_value, final_value
+):
+    """Instantiate a logarithmic schedule for a parameter.
+
+    Args:
+        decay_period: float, the period over which the value is decayed.
+        warmup_steps: int, the number of steps taken before decay starts.
+        initial_value: float, the starting value for the parameter.
+        final_value: float, the final value for the parameter.
+
+    Returns:
+        A decay function mapping step to parameter value.
+    """
+
+    start = torch.log(torch.tensor(initial_value))
+    end = torch.log(torch.tensor(final_value))
+
+    if decay_period == 0:
+        return lambda x: initial_value if x < warmup_steps else final_value
+
+    def scheduler(step):
+        step = torch.tensor(step, dtype=torch.float32)
+        steps_left = decay_period + warmup_steps - step
+        bonus_frac = steps_left / decay_period
+        bonus = torch.clamp(bonus_frac, 0.0, 1.0)
+        new_value = bonus * (start - end) + end
+
+        new_value = torch.exp(new_value)
+        return new_value.item()
+
+    return scheduler
 
